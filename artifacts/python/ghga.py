966a3a9934e252c951a7554e6d70e36ed655981a
GitHub Action <action@github.com>
2021-07-13 14:31:16 +0000

    Autogenerate artifacts

--- artifacts/python/ghga.py
+++ artifacts/python/ghga.py
@@ -1,5 +1,5 @@
 # Auto generated from ghga.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-07-13 12:12
+# Generation date: 2021-07-13 14:31
 # Schema: GHGA-Metadata-Schema
 #
 # id: https://w3id.org/GHGA-Metadata-Schema
