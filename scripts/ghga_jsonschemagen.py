82c4348e7aeeb1352ac111ae24833ff332640cd8
KerstenBreuer <kersten-breuer@outlook.com>
2023-01-24 16:34:42 +0000

    added devcontainer

--- scripts/ghga_jsonschemagen.py
+++ scripts/ghga_jsonschemagen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import click
 from ghga_metadata_utils.generators.jsonschemagen import GhgaJsonSchemaGenerator
 from linkml.utils.generator import shared_arguments
--- scripts/ghga_pydanticgen.py
+++ scripts/ghga_pydanticgen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import click
 from ghga_metadata_utils.generators.pydanticgen import GhgaPydanticGenerator
 from linkml.utils.generator import shared_arguments
