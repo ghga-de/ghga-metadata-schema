ebf6b4e66d402eb17d55a4df37279528cc15d489
sbilge <bilge.sueruen@uni-tuebingen.de>
2023-03-14 12:38:24 +0000

    ci for docs

--- scripts/generate_linkml_docs.py
+++ scripts/generate_linkml_docs.py
@@ -1,26 +1,49 @@
 #!/usr/bin/env python3
 """Script to generate linkml markdown documents"""
 
-from typing import TextIO
 from pathlib import Path
-
+import sys
+import subprocess
+from tempfile import TemporaryDirectory
 from typer import run
 from linkml.generators.markdowngen import MarkdownGenerator
 
 HERE = Path(__file__).parent.resolve()
+LINKML_YAML = HERE.parent / "src" / "schema" / "submission_centric_schema.yaml"
 DOCS_DIR = HERE.parent / "docs" / "schema_markdown"
 
 
-def generate_linkml_markdown(linkml_yaml: TextIO):
+def generate_linkml_markdown(docs_dir):
     """Function to generate markdown documentations of a given schema"""
-    return MarkdownGenerator(schema=linkml_yaml, directory_output=True).serialize(
-        directory=DOCS_DIR
+    return MarkdownGenerator(schema=str(LINKML_YAML), directory_output=True).serialize(
+        directory=docs_dir
     )
 
 
-def main(linkml_yaml):
-    """Main function"""
-    generate_linkml_markdown(linkml_yaml)
+def compare_folders(expected: str, observed: str):
+    """Function to check equality of contents of two folders"""
+    try:
+        subprocess.run(
+            ["diff", "-arg", expected, observed],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+        )
+        return "Schema documents are up to date"
+    except subprocess.CalledProcessError as exc:
+        print("Status : FAIL", exc.returncode, exc.stderr)
+        sys.exit(1)
+
+
+def main(check: bool = False):
+    """Update or check the current entity relationship diagram."""
+    generate_linkml_markdown(DOCS_DIR)
+
+    if check:
+        with TemporaryDirectory() as tmpdirname:
+            generate_linkml_markdown(tmpdirname)
+
+        compare_folders(str(DOCS_DIR), tmpdirname)
 
 
 if __name__ == "__main__":
