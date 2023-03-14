daf7979f406053cf05dc4bf5b616ceee500ea7b9
sbilge <bilge.sueruen@uni-tuebingen.de>
2023-03-14 14:24:46 +0000

    linter exit case added

--- scripts/schema_linter.py
+++ scripts/schema_linter.py
@@ -2,30 +2,33 @@
 
 """Script to employ linkml-linter"""
 import subprocess
+import sys
 from pathlib import Path
 
 
 HERE = Path(__file__).parent.resolve()
 LINTER_CONFIG = HERE.parent / ".linkml_linter.yaml"
 SCHEMA_DIR = HERE.parent / "src" / "schema"
-LINTING_DOC = HERE.parent / "docs" / "linkml_linter.md"
 
 
-def run_linter(linkml_yaml: Path, report: Path):
+def run_linter(schema_yaml: Path):
     """Function to call linkml-linter"""
-    return subprocess.Popen(
-        [
-            "linkml-lint",
-            "--config",
-            LINTER_CONFIG,
-            "--format",
-            "markdown",
-            "--output",
-            str(report),
-            str(linkml_yaml),
-        ]
-    )
+    try:
+        subprocess.run(
+            [
+                "linkml-lint",
+                "--config",
+                LINTER_CONFIG,
+                str(schema_yaml),
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        print(f"Status : FAIL, {exc.returncode}, {exc.stdout}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    run_linter(SCHEMA_DIR, LINTING_DOC)
+    run_linter(SCHEMA_DIR)
