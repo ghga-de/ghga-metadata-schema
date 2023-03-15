ce104ef932457a6056db66b84c9a6e82f38b12ae
Kersten Breuer <kersten-breuer@outlook.com>
2023-03-15 10:38:39 +0100

    make entity relation diagrams configurable (#95)
    Now the generation of diagrams can be controlled via
    the config file ".erdiagrams.yaml" in the root directory.

--- scripts/update_entity_relations.py
+++ scripts/update_entity_relations.py
@@ -1,34 +1,53 @@
 #!/usr/bin/env python3
 
-"""Update entity relation ship diagrams for LinkML yamls."""
+"""Update entity relation ship diagrams for LinkML yamls.
+This script is configured by the yaml located at the CONFIG_FILE path specified in the
+following.
+"""
 
 import sys
-import os
 import subprocess
 import difflib
 from pathlib import Path
+from typing import Optional
 
+from pydantic import BaseModel, Field
 import yaml
 
-try:
-    from script_utils.cli import echo_failure, echo_success, run
-except ImportError:
-    echo_failure = echo_success = print  # type: ignore
-
-    def run(main_fn):
-        """Run main function without cli tools (typer)."""
-        main_fn(check="--check" in sys.argv[1:])
+from script_utils.cli import echo_failure, echo_success, run
 
 
 HERE = Path(__file__).parent.resolve()
+CONFIG_FILE = HERE.parent / ".erdiagrams.yaml"
 SCHEMA_DIR = HERE.parent / "src" / "schema"
-SUMMARY_DOC = HERE.parent / "docs" / "entity_relations.md"
+OUTPUT_FILE = HERE.parent / "docs" / "entity_relations.md"
+
+
+class ErDiagramJob(BaseModel):
+    """Model containing instructions to create an Entity Relationship Diagram."""
+
+    title: str = Field(..., description="The title of the diagram.")
+    description: str = Field(..., description="Description of the diagram.")
+    model: str = Field(
+        ...,
+        description="The filename of the model file is targeted.",
+    )
+    classes: Optional[list[str]] = Field(
+        None, description="Classes to include. If not specified all classes are shown."
+    )
+    tree_root: bool = Field(
+        False,
+        description="Whether or not to only show classes connected to the tree root.",
+    )
+    include_attributes: bool = Field(
+        False, description="Whether or not to include attributes."
+    )
 
 
 def fix_enum_line(line: str):
     """Fix problem with enum line."""
 
-    slot, _, target = line.partition("enum")
+    slot, _, target = line.partition(" enum ")
     fixed_slot = slot.strip().replace(" ", "_")
     return f"    string {fixed_slot}"
 
@@ -38,17 +57,45 @@ def fix_enum_problems(*, erd_diagram: str) -> str:
 
     return "\n".join(
         [
-            fix_enum_line(line) if "enum" in line else line
+            fix_enum_line(line) if " enum " in line else line
             for line in erd_diagram.splitlines()
         ]
     )
 
 
-def get_detailed_erd_diagram(*, linkml_yaml: Path) -> str:
-    """Get a detailed erd diagram for the provided model.."""
+def get_erd_command(*, job: ErDiagramJob) -> list[str]:
+    """Get a command line to generate an ERD using the specified job."""
+
+    schema_path = SCHEMA_DIR / job.model
+
+    command = ["gen-erdiagram"]
+
+    if job.tree_root:
+        command.append("--structural")
+
+    if not job.include_attributes:
+        command.append("--exclude-attributes")
+
+    if job.classes:
+        command.append("--max-hops")
+        command.append("0")
+
+        for cls in job.classes:
+            command.append("--classes")
+            command.append(cls)
+
+    command.append(str(schema_path))
+
+    return command
+
+
+def get_erd_diagram(*, job: ErDiagramJob) -> str:
+    """Get a detailed ERD for the provided model.."""
+
+    command = get_erd_command(job=job)
 
     with subprocess.Popen(
-        ["gen-erdiagram", str(linkml_yaml)],
+        command,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     ) as process:
@@ -56,110 +103,81 @@ def get_detailed_erd_diagram(*, linkml_yaml: Path) -> str:
 
         if process.wait() != 0:
             raise RuntimeError(
-                f"Could not generate erd diagram for model '{linkml_yaml}',"
+                f"Could not generate erd diagram for model '{job.model}',"
                 + f" the error was: {stderr.decode('utf-8')}"
             )
 
     erd_diagram = stdout.decode("utf-8")
-    return fix_enum_problems(erd_diagram=erd_diagram)
+
+    if job.include_attributes:
+        erd_diagram = fix_enum_problems(erd_diagram=erd_diagram)
+
+    return erd_diagram
 
 
-def get_basic_erd_diagram(*, linkml_yaml: Path) -> str:
-    """Get a basic erd diagram for the provided model."""
+def get_diagram_header(job: ErDiagramJob) -> str:
+    """Get a title and description for the specified diagram job in markdown."""
 
-    with subprocess.Popen(
-        ["gen-erdiagram", "--exclude-attributes", "--structural", str(linkml_yaml)],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    ) as process:
-        stdout, stderr = process.communicate()
-
-        if process.wait() != 0:
-            raise RuntimeError(
-                f"Could not generate erd diagram for model '{linkml_yaml}',"
-                + f" the error was: {stderr.decode('utf-8')}"
-            )
-
-    return stdout.decode("utf-8")
+    return f"## {job.title}\n\n{job.description}  \n\n"
 
 
-def get_model_name(*, linkml_yaml: Path) -> str:
-    """Extract the name of the specified linkml model."""
+def get_annotated_diagram(*, job: ErDiagramJob) -> str:
+    """Get an ERD with a title and a description in markdown."""
 
-    with open(linkml_yaml, "r", encoding="utf-8") as file:
-        linkml_dict = yaml.safe_load(file)
+    diagram_header = get_diagram_header(job=job)
+    diagram = get_erd_diagram(job=job)
 
-    return linkml_dict["name"]
-
-
-def get_schema_paths() -> list[Path]:
-    """Returns a list of paths to the models."""
-
-    paths = [
-        SCHEMA_DIR / filename
-        for filename in os.listdir(SCHEMA_DIR)
-        if filename.endswith(".yaml")
-    ]
-
-    return sorted(paths)
+    return diagram_header + diagram
 
 
-def generate_doc(*, linkml_yaml: Path) -> str:
-    """Returns a markdown-based erd diagram for the specified linkml yaml."""
+def generate_doc(*, jobs: list[ErDiagramJob]) -> str:
+    """Returns a markdown-based doc that contains all erd diagram."""
 
-    model_name = get_model_name(linkml_yaml=linkml_yaml)
-    detailed_erd_diagram = get_detailed_erd_diagram(linkml_yaml=linkml_yaml)
-    basic_erd_diagram = get_basic_erd_diagram(linkml_yaml=linkml_yaml)
+    diagrams = [get_annotated_diagram(job=job) for job in jobs]
 
-    return (
-        f"## {model_name}\n\n"
-        f"### Basic Diagram:\n\n{basic_erd_diagram}\n\n"
-        f"### Detailed Diagram:\n\n{detailed_erd_diagram}"
-    )
+    return "# Entity Relationship Diagrams\n\n" + "\n\n".join(diagrams)
 
 
-def generate_summary_doc() -> str:
-    """Returns a markdown-based doc that contains erd diagrams for all models."""
+def write_doc(doc: str) -> None:
+    """Write a doc to the corresponding markdown file."""
 
-    linkml_yamls = get_schema_paths()
-
-    docs_per_yaml = [
-        generate_doc(linkml_yaml=linkml_yaml) for linkml_yaml in linkml_yamls
-    ]
-
-    return "# Entity Relationship Diagrams\n\n" + "\n\n".join(docs_per_yaml)
-
-
-def write_summary_doc(doc: str) -> None:
-    """Write a summary doc to the corresponding markdown file."""
-
-    with open(SUMMARY_DOC, "w", encoding="utf-8") as file:
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as file:
         file.write(doc)
 
 
-def read_summary_doc() -> str:
-    """Read the summary doc markdown file."""
+def read_doc() -> str:
+    """Read the current markdown file."""
 
-    with open(SUMMARY_DOC, "r", encoding="utf-8") as file:
+    with open(OUTPUT_FILE, "r", encoding="utf-8") as file:
         return file.read()
+
+
+def get_jobs() -> list[ErDiagramJob]:
+    """Read jobs from the config yaml."""
+
+    with open(CONFIG_FILE, "r", encoding="utf-8") as file:
+        jobs = yaml.safe_load(file)
+
+    return [ErDiagramJob(**job) for job in jobs]
 
 
 def main(check: bool = False):
     """Update or check the current entity relationship diagram."""
 
-    expected_summary_doc = generate_summary_doc()
+    jobs = get_jobs()
+    expected_doc = generate_doc(jobs=jobs)
 
     if check:
-        observed_summary_doc = read_summary_doc()
+        observed_doc = read_doc()
 
-        if expected_summary_doc == observed_summary_doc:
+        if expected_doc == observed_doc:
             echo_success("Entity relationship diagrams are up to date.")
             return
 
         print(f"Observed file differs from the expected one:")
         for line in difflib.unified_diff(
-            observed_summary_doc.splitlines(keepends=True),
-            expected_summary_doc.splitlines(keepends=True),
+            expected_doc.splitlines(keepends=True),
+            observed_doc.splitlines(keepends=True),
             fromfile="observed",
             tofile="expected",
         ):
@@ -168,7 +186,7 @@ def main(check: bool = False):
         echo_failure("Entity relationship diagrams are not up to date.")
         sys.exit(1)
 
-    write_summary_doc(expected_summary_doc)
+    write_doc(expected_doc)
     echo_success("Successfully updated the entity relationship diagram.")
 
 
